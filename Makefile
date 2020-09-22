82a217c99c0af55ed6b0a64a356ca839715b85e5
Alan Fernando Rincón Vieyra <arinconv@everis.com>
2020-09-21 23:43:53 -0500

    Ini breakdown

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
-PROJ = find-sha256-brute-force
-MAIN = find-sha256-brute-force.c
+PROJ = sha256-breakdown.a
+MAIN = sha256-breakdown.c
 OBJS = lib/sha256.o lib/utils.o
 CFLAGS = -std=c99 -O3
 LFLAGS = -Ilib
 CC = gcc
 
+MESSAGE = 50aa1d9bfd848b6560edcdb0a95ee3fa0ff36b8cb6cdc6a5ee0dfdc41087dc56
 HASH = 4852c8159c4fcde8444ec454acc21381680c990f2fc611e8a713513b494e6bc5
 
 $(PROJ): $(MAIN) $(OBJS)
@@ -14,7 +15,7 @@ $(OBJS): %.o: %.c
 	$(CC) $(CFLAGS) -c -Wall -o $@ $<
 
 run: $(PROJ)
-	./$(PROJ) $(HASH)
+	./$(PROJ) $(MESSAGE)
 
 clean:
 	rm -rf $(PROJ) *.o **/*.o
--- sha256-breakdown.c
+++ sha256-breakdown.c
@@ -12,6 +12,10 @@ void padding(uint8_t out[]) {
   out[126] = 2;
 }
 
+void print_state() {
+  
+}
+
 char* hash_msg(uint8_t message[128], uint8_t hash[32]) {
   /* initial state */
   uint32_t state[8] = {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
@@ -27,53 +31,29 @@ char* hash_msg(uint8_t message[128], uint8_t hash[32]) {
   }
 }
 
-void gen_msg(uint8_t msg[128]) {
-  // char test[64] =
-  //     "50aa1d9bfd848b6560edcdb0a95ee3fa0ff36b8cb6cdc6a5ee0dfdc41087dc56";
-  char characters[16] = "0123456789abcdef";
-
-  for (uint8_t i = 0; i < 64; i++) {
-    msg[i] = characters[rand() % 16];
-  }
-}
-
-void find_hash(const char* hash_str) {
-  uint8_t hash_target[32];
+void breakdown(const char message[64]) {
   uint8_t msg_padded[128];
   uint8_t hash_result[32];
 
-  hex_to_bytes(hash_str, hash_target);
-  // printf("Hash: %s\n", bytes_to_hex(hash_target, 32));
+  memcpy(msg_padded, message, 64);
   padding(msg_padded);
-  // printf("Padding: %s\n", bytes_to_hex(msg_padded, 128));
 
-  while (1) {
-    gen_msg(msg_padded);
-    hash_msg(msg_padded, hash_result);
+  hash_msg(msg_padded, hash_result);
 
-    if (!memcmp(hash_result, hash_target, 32)) {
-      msg_padded[64] = 0;
-      printf("Complete.\nMessage: %s\n", msg_padded);
-
-      printf("SHA256 hash: ");
-      printf("%02X%02X%02X...%02X%02X%02X\n", hash_result[0], hash_result[1],
+  printf("SHA256 hash: %02X%02X%02X...%02X%02X%02X\n", hash_result[0], hash_result[1],
             hash_result[2], hash_result[29], hash_result[30], hash_result[31]);
-      break;
-    }
-  }
 }
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
     fprintf(
         stderr,
-        "Example: ./find-message "
-        "4852c8159c4fcde8444ec454acc21381680c990f2fc611e8a713513b494e6bc5\n");
+        "Example: ./sha256-breakdown "
+        "50aa1d9bfd848b6560edcdb0a95ee3fa0ff36b8cb6cdc6a5ee0dfdc41087dc56\n");
     return 1;
   }
 
-  srand(time(NULL));
-  find_hash(argv[1]);
+  breakdown(argv[1]);
 
   return 0;
 }
